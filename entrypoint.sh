1f72019581689a1c69fd7e51bb641af62117fe6f
Nikolai Zujev <n.zujev@foodpanda.com>
2016-06-20 14:42:26 +0200

    fix ENTRPOINT, as it resets CMD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-set -xe
+set -e
+
+echo "* Entrypoint hook for '$@'"
 
 PHP_INI_FILE="${PHP_INI_DIR}/php.ini"
 
@@ -9,13 +11,13 @@ sed -i "/^;*date.timezone\s*=/c\date.timezone = ${PHP_INI_TIMEZONE}" ${PHP_INI_F
 sed -i "/^;*xdebug.remote_port\s*=/c\xdebug.remote_port = ${XDEBUG_REMOTE_PORT}" ${PHP_INI_FILE}
 
 if [ -z "${NEWRELIC_LICENSE}" ]; then
-	echo "NewRelic: disabled"
+	echo "* NewRelic: disabled"
 
 	sed -i "/^;*newrelic.enabled\s*=/c\;newrelic.enabled =" ${PHP_INI_FILE}
 	sed -i "/^;*newrelic.license\s*=/c\;newrelic.license =" ${PHP_INI_FILE}
 	sed -i "/^;*newrelic.appname\s*=/c\;newrelic.appname =" ${PHP_INI_FILE}
 else
-	echo "NewRelic: enabled (${NEWRELIC_APPNAME})"
+	echo "* NewRelic: enabled (${NEWRELIC_APPNAME})"
 
 	sed -i "/^;*newrelic.enabled\s*=/c\newrelic.enabled = true" ${PHP_INI_FILE}
 	sed -i "/^;*newrelic.license\s*=/c\newrelic.license = '${NEWRELIC_LICENSE}'" ${PHP_INI_FILE}
